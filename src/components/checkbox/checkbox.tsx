646a89d1d943169610c2166bd642bfd73db18757
yororoA <3364817735@qq.com>
2026-03-19 10:38:57 +0800

    fix: 组件参数补齐

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -8,6 +8,7 @@ import {
   FieldLegend,
   FieldSet,
 } from "@/components/ui/field"
+import { cn } from "@/lib/utils"
 import React from "react"
 
 type CheckedState = boolean | "indeterminate"
@@ -18,10 +19,19 @@ interface Checkbox4uItem {
   orientation?: "horizontal" | "vertical"
   content: React.ReactNode
   description?: React.ReactNode
+  className?: string
+  contentClassName?: string
+  labelClassName?: string
+  descriptionClassName?: string
   checked?: CheckedState
   defaultChecked?: boolean
   onCheckedChange?: (checked: CheckedState) => void
   disabled?: boolean
+  fieldProps?: React.ComponentProps<typeof Field>
+  contentProps?: React.ComponentProps<typeof FieldContent>
+  labelProps?: React.ComponentProps<typeof FieldLabel>
+  descriptionProps?: React.ComponentProps<typeof FieldDescription>
+  checkboxProps?: Omit<React.ComponentProps<typeof Checkbox>, "id" | "name" | "checked" | "defaultChecked" | "onCheckedChange" | "disabled">
 }
 
 /** Checkbox4u 组件参数。 */
@@ -32,29 +42,39 @@ interface Checkbox4uProps {
   }
   className?: string
   groupClassName?: string
+  setProps?: React.ComponentProps<typeof FieldSet>
+  groupProps?: React.ComponentProps<typeof FieldGroup>
+  legendProps?: React.ComponentProps<typeof FieldLegend>
+  legendDescriptionProps?: React.ComponentProps<typeof FieldDescription>
   items: Checkbox4uItem[]
 }
 
 export function Checkbox4u(props: Checkbox4uProps) {
   return (
-    <FieldSet className={props.className}>
+    <FieldSet {...props.setProps} className={cn(props.className, props.setProps?.className)}>
       {props.legend && (
         <>
-          <FieldLegend>{props.legend.content}</FieldLegend>
+          <FieldLegend {...props.legendProps}>{props.legend.content}</FieldLegend>
           {props.legend.description && (
-            <FieldDescription>
+            <FieldDescription {...props.legendDescriptionProps}>
               {props.legend.description}
             </FieldDescription>
           )}
         </>
       )}
-      <FieldGroup className={props.groupClassName ?? "mx-auto w-72"}>
+      <FieldGroup {...props.groupProps} className={cn(props.groupClassName ?? "mx-auto w-72", props.groupProps?.className)}>
         {props.items.map((item, index) => {
           const itemId = item.id ?? `checkbox-${index}`
 
           return (
-            <Field key={itemId} orientation={item.orientation ?? "horizontal"}>
+            <Field
+              key={itemId}
+              {...item.fieldProps}
+              orientation={item.fieldProps?.orientation ?? item.orientation ?? "horizontal"}
+              className={cn(item.className, item.fieldProps?.className)}
+            >
               <Checkbox
+                {...item.checkboxProps}
                 id={itemId}
                 name={itemId}
                 checked={item.checked}
@@ -62,12 +82,22 @@ export function Checkbox4u(props: Checkbox4uProps) {
                 onCheckedChange={item.onCheckedChange}
                 disabled={item.disabled}
               />
-              <FieldContent>
-                <FieldLabel htmlFor={itemId}>
+              <FieldContent
+                {...item.contentProps}
+                className={cn(item.contentClassName, item.contentProps?.className)}
+              >
+                <FieldLabel
+                  htmlFor={itemId}
+                  {...item.labelProps}
+                  className={cn(item.labelClassName, item.labelProps?.className)}
+                >
                   {item.content}
                 </FieldLabel>
                 {item.description && (
-                  <FieldDescription>
+                  <FieldDescription
+                    {...item.descriptionProps}
+                    className={cn(item.descriptionClassName, item.descriptionProps?.className)}
+                  >
                     {item.description}
                   </FieldDescription>
                 )}
