670e19fb1695f812ffe7ab6170caef1056e27cbb
yororoA <3364817735@qq.com>
2026-03-29 15:45:59 +0800

    update: 增添消息提取工具

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { AdminAgentMessage } from "@/app/api/chat/model";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -8,4 +9,10 @@ export function cn(...inputs: ClassValue[]) {
 export function strToHexStr(str: string): string {
   return new TextEncoder().encode(str)
     .reduce((acc, byte) => acc + byte.toString(16).padStart(2, '0'), '')
+}
+
+export function getShowResponsePayload(message: AdminAgentMessage) {
+  return message.parts
+    .find((part) => part.type === "tool-showResponse")
+    ?.input;
 }
